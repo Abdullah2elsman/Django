b09f23146e5da10857e6b931c6110441afcb1f3d
Abdullah2elsman <abdullahelsman294@gmail.com>
2026-04-29 02:59:51 +0300

    admin page

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "is_bestselling")
 # Register your models here.
+admin.site.register(Book, BookAdmin)
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,7 +8,7 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=200)
     is_bestselling = models.BooleanField(default=False)
-    slug= models.SlugField(default="", null=False, db_index=True)
+    slug= models.SlugField(default="", null=False, db_index=True, blank=True)
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
 
